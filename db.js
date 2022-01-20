71065b2b4e0d8aab9fcd99c5f2c6a4a67e3b4e6c
Jared Hanson <jaredhanson@gmail.com>
2022-01-19 18:33:50 -0800

    Create users table in database.

--- db.js
+++ db.js
@@ -1,16 +1,32 @@
 var sqlite3 = require('sqlite3');
 var mkdirp = require('mkdirp');
+var crypto = require('crypto');
 
 mkdirp.sync('var/db');
 
 var db = new sqlite3.Database('var/db/todos.db');
 
 db.serialize(function() {
+  // create the database schema for the todos app
+  db.run("CREATE TABLE IF NOT EXISTS users ( \
+    username TEXT UNIQUE, \
+    hashed_password BLOB, \
+    salt BLOB \
+  )");
+  
   db.run("CREATE TABLE IF NOT EXISTS todos ( \
     owner_id INTEGER NOT NULL, \
     title TEXT NOT NULL, \
     completed INTEGER \
   )");
+  
+  // create an initial user (username: alice, password: letmein)
+  var salt = crypto.randomBytes(16);
+  db.run('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+    'alice',
+    crypto.pbkdf2Sync('letmein', salt, 310000, 32, 'sha256'),
+    salt
+  ]);
 });
 
 module.exports = db;
