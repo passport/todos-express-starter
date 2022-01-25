cef35e3f5fd85691ca702333b2a6e42486eea249
Jared Hanson <jaredhanson@gmail.com>
2022-01-25 09:15:34 -0800

    Add name column to users table.

--- db.js
+++ db.js
@@ -11,7 +11,8 @@ db.serialize(function() {
   db.run("CREATE TABLE IF NOT EXISTS users ( \
     username TEXT UNIQUE, \
     hashed_password BLOB, \
-    salt BLOB \
+    salt BLOB, \
+    name TEXT \
   )");
   
   db.run("CREATE TABLE IF NOT EXISTS federated_credentials ( \
